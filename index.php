bab4664af08bbff07dac69ac2c3094edcfdb05d2
LucieDAC <lucie.d.arsonval@gmail.com>
2017-05-29 20:44:52 +0200

    unresponsive menu half way

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Main - edhbr</title>
-  <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet">
+  <title>Stairnet - Accueil</title>
+  <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville|Open+Sans" rel="stylesheet"> 
   <link rel="stylesheet" href="styles/style.css" />
   <noscript><style>.jsonly{ display: none }</style></noscript>  
 </head>
 <body>
   <?php include("views/header/header.php") ?>
-  
+
   <?php include("views/footer/footer.php") ?>
   <!-- scripts -->
   <script type="text/javascript" src="scripts/main.js"></script>
---
+++ views/header/header.php
@@ -0,0 +1,23 @@
+<header id="header">
+	<a href="index.php" id="logo">
+		<img src="images/stairnetLogo.svg" alt="Stairnet Logo">
+		<div class="logoTitle">
+			Stairnet
+			<div>Nettoyage</div>
+		</div>
+	</a>
+	<nav id="navHeader">
+		<ul>
+			<li class="home navItem0"><a href="#">Accueil</a></li>
+			<li class="services navItem1">
+				<a href="#">Services</a>
+				<ul>
+					<li>Locaux Professionnels</li>
+					<li>Copropriété - Commun d'Immeuble</li>
+					<li>Services Ponctuels</li>
+				</ul>
+			</li>
+			<li class="contact navItem2"><a href="#">Contact</a></li>
+		</ul>
+	</nav>
+</header>
